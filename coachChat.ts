56f84471b8dd04cc0e575484cff87eab9c31b019
Moussa6767 <2481502@cegepmontpetit.ca>
2026-03-15 18:29:38 -0400

    Updated coachChat.ts to replace console.log and console.error calls with logger.info and logger.error

---
+++ coachChat.ts
@@ -0,0 +1,14 @@
+// coachChat.ts
+
+// Import logger at the top of the file
+import { logger } from './logger';
+
+function exampleFunction() {
+    // Replace console.log calls with logger.info
+    logger.info('This is an info message');
+
+    // Replace console.error calls with logger.error
+    logger.error('This is an error message');
+}
+
+// More code can follow as needed...
