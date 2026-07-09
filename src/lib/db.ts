8cc0f8c0bd6ef6077747a8e51f0753836d341655
Z User <z@container>
2026-07-09 04:29:11 +0000

    fix: export PrismaClient instance instead of getDb function from db.ts
    All routes use db.player.findUnique() etc. expecting a PrismaClient
    instance, but db.ts was exporting getDb (a function).

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,1 +1,6 @@
-export { getDb as db, healthCheck, disconnect, POOL_CONFIG, SLOW_QUERY_THRESHOLD_MS } from './database'
+import { getDb } from './database'
+
+/** Prisma client singleton instance. Safe to import and use directly. */
+export const db = getDb()
+
+export { healthCheck, disconnect, POOL_CONFIG, SLOW_QUERY_THRESHOLD_MS } from './database'
